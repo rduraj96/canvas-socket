fa4767b866bf4d92f5f4768225d6b84ce451d67b
rduraj96 <durajrei@gmail.com>
2023-06-29 04:54:31 -0400

    Initial Commit

---
+++ hooks/useDraw.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useDraw = (
+  onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void
+) => {
+  const [mouseDown, setMouseDown] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const prevPoint = useRef<Point | null>(null);
+
+  const onMouseDown = () => setMouseDown(true);
+
+  useEffect(() => {
+    const mouseMoveHandler = (e: MouseEvent) => {
+      if (!mouseDown) return;
+      const currentPoint = computePointInCanvas(e);
+
+      const ctx = canvasRef.current?.getContext("2d");
+      if (!ctx || !currentPoint) return;
+
+      onDraw({ ctx, currentPoint, prevPoint: prevPoint.current });
+      prevPoint.current = currentPoint;
+    };
+
+    const computePointInCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      return { x, y };
+    };
+
+    const mouseUpHandler = () => {
+      setMouseDown(false);
+      prevPoint.current = null;
+    };
+
+    canvasRef.current?.addEventListener("mousemove", mouseMoveHandler);
+    window.addEventListener("mouseup", mouseUpHandler);
+
+    return () => {
+      canvasRef.current?.removeEventListener("mousemove", mouseMoveHandler);
+      window.removeEventListener("mouseup", mouseUpHandler);
+    };
+  }, [onDraw]);
+
+  return { canvasRef, onMouseDown };
+};
